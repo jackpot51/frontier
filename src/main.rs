6ed130eb787154c3ac5d7a492cf7199cb596f5d0
Jeremy Soller <jackpot51@gmail.com>
2016-08-26 17:01:43 -0600

    Add better font, title underlined

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use orbfont::Font;
 
 fn main(){
     let mut window = Window::new(100, 100, 640, 480, "Frontier").unwrap();
-    let font = Font::find(None, None, None).unwrap();
+    let font = Font::from_path("res/FiraMono-Regular.ttf").unwrap();
 
     let ship = starship::load("res/ship.json").unwrap();
 
@@ -20,16 +20,26 @@ fn main(){
     'events: loop {
         let deck = &ship.decks[deck_i];
 
+        let window_w = window.width();
+        let window_h = window.height();
+
         window.set(Color::rgb(255, 255, 255));
-        font.render(&format!("{} - {} - {}", ship.name, deck_i, deck.name), 16.0).draw(&mut window, 0, 0, Color::rgb(0, 0, 0));
+
+        let title = font.render(&format!("{} - {} - {}", ship.name, deck_i, deck.name), 24.0);
+        let title_x = (window_w - title.width()) as i32/2;
+        title.draw(&mut window, title_x, 0, Color::rgb(0, 0, 0));
+
+        window.rect(0, 26, window_w, 2, Color::rgb(0, 0, 0));
+
         for block in deck.blocks.iter() {
             let x = block.x as i32 * 32;
-            let y = block.y as i32 * 32 + 16;
+            let y = block.y as i32 * 32 + 32;
             let w = block.w as u32 * 32;
             let h = block.h as u32 * 32;
             window.rect(x, y, w, h, Color::rgb(128, 128, 128));
             font.render(&block.kind, 16.0).draw(&mut window, x, y, Color::rgb(0, 0, 0));
         }
+
         window.sync();
 
         for event in window.events() {
